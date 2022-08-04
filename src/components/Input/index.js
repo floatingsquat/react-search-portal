d3cce5ccd5b5b657dc260cbbc97be3c910f6f9aa
Murat YILMAZ <muratyilmaz1634@gmail.com>
2022-08-04 15:20:42 +0300

    Make it red if the inputs are empty

--- src/components/Input/index.js
+++ src/components/Input/index.js
@@ -2,10 +2,19 @@ import React from "react";
 import styles from "./styles.module.scss";
 function Input({ icon = false, error, label, placeholder, ...rest }) {
   return (
-    <div className={styles.inputWrapper}>
-      {label && <label>{label}</label>}
-      <input {...rest} placeholder={placeholder} />
-      {error && <label>{error}</label>}
+    <div className={`${styles.inputWrapper} ${error && styles.error}`}>
+      {/* className={`${error ? styles.inputWrapperError : styles.inputWrapper}`} */}
+
+      {/* className={`${styles.page} ${
+            page === currentPage ? styles.active : ""
+          }`} */}
+      {label && <label className={`${error && styles.error}`}>{label}</label>}
+      <input
+        className={`${error && styles.errorInput}`}
+        {...rest}
+        placeholder={placeholder}
+      />
+      {error && <label className={`${error && styles.error}`}>{error}</label>}
     </div>
   );
 }
